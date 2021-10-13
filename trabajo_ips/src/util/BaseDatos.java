fc630b4f6ba97fdecb73071d3bc2a2706343384b
natal <natal@LAPTOP-BLQTLTAD>
2021-10-13 17:45:10 +0200

    4

--- trabajo_ips/src/util/BaseDatos.java
+++ trabajo_ips/src/util/BaseDatos.java
@@ -7,7 +7,7 @@ import java.sql.Statement;
 
 public class BaseDatos {
 
-//uasdhkawdsfdfdasddasdj
+//v
 	private static String DRIVER = "org.hsqldb.jdbcDriver";
 	private static String URL = "jdbc:hsqldb:hsql://localhost";
 	private static String USER = "sa";
