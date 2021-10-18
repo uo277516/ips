d392093f7daf64c16d77121e60ab9d80ee426890
joser <joser@LAPTOP-59RJKCU5>
2021-10-18 17:55:33 +0200

    c

--- trabajo_ips/src/igu/VentanaMostrarCarreras.java
+++ trabajo_ips/src/igu/VentanaMostrarCarreras.java
@@ -210,7 +210,18 @@ public class VentanaMostrarCarreras extends JFrame {
 			table.setFont(new Font("Tahoma", Font.PLAIN, 13));
 			table.setSelectionBackground(Color.YELLOW);
 			table.setBackground(Color.LIGHT_GRAY);
-			DefaultTableModel modelo = new DefaultTableModel();
+			DefaultTableModel modelo = new DefaultTableModel() {
+				 /**
+                 * 
+                 */
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public boolean isCellEditable(int row, int column) {
+                    return false;
+                }
+           
+			};
 			table.setModel(modelo);
 			modelo.addColumn("Iden");modelo.addColumn("Nombre");modelo.addColumn("Fecha Comp");modelo.addColumn("Tipo");modelo.addColumn("Distancia");modelo.addColumn("Cuota");modelo.addColumn("Fecha Fin Insc");modelo.addColumn("Plazas");
 			List<CompeticionDto> competiciones = comp.getCompetcionesFechaLista(textFecha.getText());
@@ -227,6 +238,7 @@ public class VentanaMostrarCarreras extends JFrame {
 				modelo.addRow(info[i]);
 			}
 		}
+		
 		
 		return table;
 	}
